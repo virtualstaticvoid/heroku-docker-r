7794b1a5da61e1cdcd80a76c46dbc6f8092083d7
Chris Stefano <virtualstaticvoid@gmail.com>
2022-07-10 10:28:52 +0100

    update tests
    `BASIX` package is no longer available

--- test/units/test.R
+++ test/units/test.R
@@ -1,13 +1,9 @@
 # HTTP mirror to support R 3.1
 options(repos = c("https://cloud.r-project.org", "http://cloud.r-project.org"))
 
-# Install a package without compilation
-install.packages("R6", quiet=TRUE)
+# Check that packages can be installed
+install.packages("R6")
 library(R6)
-
-# Install a package with compilation
-install.packages("BASIX", quiet=TRUE)
-library(BASIX)
 
 # Check that the time zone database is present
 # https://stat.ethz.ch/R-manual/R-devel/library/base/html/timezones.html
